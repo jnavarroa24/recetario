40eefe55d3d84d78814cc01bce5ce99e7155e623
Javier <javierasiraldebaran@gmail.com>
2026-06-16 01:58:01 +0200

    mapper

---
+++ src/main/java/com/javier/recetario/infraestructure/persistence/mapper/IngredientPersistenceMapper.java
@@ -0,0 +1,13 @@
+package com.javier.recetario.infraestructure.persistence.mapper;
+
+import com.javier.recetario.domain.model.Ingredient;
+import com.javier.recetario.infraestructure.persistence.IngredientDocument;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface IngredientPersistenceMapper {
+
+    Ingredient toDomain(IngredientDocument document);
+
+    IngredientDocument toDocument(Ingredient ingredient);
+}
